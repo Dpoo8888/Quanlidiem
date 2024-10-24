2c6a5bf7fdd0ef75ec23952654b268dd0d1e42ff
Duong <Duong@gmail.com>
2024-10-24 12:05:33 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+require_once 'C:\xampp\htdocs\php_mvc-master\connect\connect.php';
+
+$database = new Database();
+$data = $database->Connect();
+if (isset($_GET['controller'])) {
+	$controller = $_GET['controller'];
+}
+else
+{
+	$controller = NULL;
+}
+
+switch ($controller) {
+	case 'DangNhap':
+		require_once 'C:\xampp\htdocs\php_mvc-master\controller\DangNhap.php';
+		break;
+	case 'ThongTinCaNhanHS':
+		require_once 'C:\xampp\htdocs\php_mvc-master\controller\DiemMonHoc.php';
+		break;
+	case 'ThongTinCaNhanGV':
+		require_once 'D:/Code/quanly_diem/controller/ThongTinCaNhanGV.php';
+		break;
+	case 'QuanLyDiem':
+		require_once 'D:/Code/quanly_diem/controller/DiemMonHoc.php';
+		break;
+	case 'QuanLyDaoTao':
+		require_once 'D:/Code/quanly_diem/controller/QuanLyDaoTao.php';
+		break;
+	case 'ThongTinHocSinh':
+		require_once 'D:/Code/quanly_diem/controller/ThongTinHocSinh.php';
+		break;
+	case 'ThongTinGiaoVien':
+		require_once 'D:/Code/quanly_diem/controller/ThongTinGiaoVien.php';
+		break;
+	default:
+		require_once 'D:/Code/quanly_diem/controller/DangNhap.php';
+		break;
+}
+?>
